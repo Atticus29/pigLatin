59507e6c3a76f879a8f319eb6b684b50528c88d5
Mark <mark.aaron.fisher@gmail.com>
2017-02-22 11:12:15 -0800

    got translateToPigLatin function to move an initial consonant to the end of the word and then add ay at the end

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
 // Back End
+var moveFirstLetterToEnd = function(str){
+  str = str + str[0];
+  str = str.substring(1);
+  return str;
+}
+
 var translateToPigLatin = function (str){
-  var translated = str + "ay";
+  var vowels = ["a", "e", "i", "o", "u"];
+  var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
+  var firstLetter = str[0];
+  var translated = str;
+  if (consonants.indexOf(firstLetter)>-1){
+    translated = moveFirstLetterToEnd(str);
+  }
+  translated = translated + "ay";
   return translated;
 }
 // Front End
